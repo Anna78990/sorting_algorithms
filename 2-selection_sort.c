72dd26226e6a6e22badaa2e0d42bbe8a7be5f430
Anna78990 <takasuanna1990@gmail.com>
2022-02-15 14:23:53 +0000

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,8 +1,9 @@
 #include <stddef.h>
+#include <stdio.h>
 #include "sort.h"
 
 /**
- * quick_sort - quick_sort
+ * selection_sort - selection sort
  * @array: array to sort
  * @size: length of array
  * Return: Nothing
@@ -11,7 +12,7 @@
 void selection_sort(int *array, size_t size)
 {
 	int i, j, min_index;
-	
+
 	for (i = 0; i < (int)size - 1; i++)
 	{
 		min_index = i;
@@ -23,14 +24,21 @@ void selection_sort(int *array, size_t size)
 			}
 		}
 		swap_elements(&array[min_index], &array[i]);
+		print_array(array, size);
 	}
 }
+
+/**
+ * swap_elements - swap the elements
+ * @x: element1
+ * @y: element2
+ */
 
 void swap_elements(int *x, int *y)
 {
 	int tmp;
 
 	tmp = *x;
-	*y = tmp;
 	*x = *y;
+	*y = tmp;
 }
